80942b0056ee78ddb3eab617792a4870a1c3341a
d9k <d9k@ya.ru>
2015-08-22 07:47:26 +0300

    python reStructured text type docstring trial

--- server/best_tests_server/models.py
+++ server/best_tests_server/models.py
@@ -1,17 +1,17 @@
-# coding: utf-8
+import uuid
 from sqlalchemy import BigInteger, Column, Integer, Text
 from sqlalchemy.ext.declarative import declarative_base
 from hashlib import sha256
-import uuid
+from zope.sqlalchemy import ZopeTransactionExtension
 
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
 )
 
-from zope.sqlalchemy import ZopeTransactionExtension
-
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+""":type: sqlalchemy.orm.Session """
+
 Base = declarative_base()
 
 GROUPS = {'admin': ['group:admins']}
@@ -53,9 +53,8 @@ class User(Base):
     # stores password hash and salt separated by colon
     password_hash = Column(Text)
 
-    # TODO proper docstrings
-    # thx 2 @see http://pythoncentral.io/hashing-strings-with-python/
     def set_password(self, password):
+        """thx 2 http://pythoncentral.io/hashing-strings-with-python"""
         salt = create_salt()
         self.password_hash = create_hashed_password(password, salt) + ':' + salt
 
@@ -63,5 +62,21 @@ class User(Base):
         hashed_password_only, salt = self.password_hash.split(':')
         return hashed_password_only == create_hashed_password(user_password, salt)
 
-    # @staticmethod
-    # TODO def groupfinder(user_id):
+    @staticmethod
+    def get_groups(user_id):
+        user = User.by_id(user_id)
+        """:type: User"""
+        if not user:
+            return []
+        if user.group not in GROUPS:
+            return []
+        return GROUPS[user.group]
+
+
+    @staticmethod
+    def by_id(user_id):
+        """
+        :param user_id: int
+        :return: User
+        """
+        return DBSession.query(User).filter(User.id == user_id).first()

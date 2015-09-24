974fde5e76d729a5dbea45460b34e10ce7b410c8
d9k <d9k@ya.ru>
2015-09-24 15:30:05 +0300

    recaptcha 2->3 (needs debug)

--- server/best_tests_server/widgets.py
+++ server/best_tests_server/widgets.py
@@ -4,7 +4,7 @@ from deform.widget import CheckedInputWidget
 from colander import null, Invalid
 from pyramid.threadlocal import get_current_request
 
-from urllib.parse import urlencode
+from urllib.parse import urlencode, urlparse
 import http.client
 
 # @colander.deferred
@@ -12,13 +12,15 @@ import http.client
 #     request = kw['request']
 
 # from https://gist.github.com/reedobrien/701444
+# TODO https://docs.python.org/3.2/library/http.client.html
 
 class RecaptchaWidget(CheckedInputWidget):
     template = 'recaptcha_widget'
     readonly_template = 'recaptcha_widget'
     requirements = ()
     url = "http://www.google.com/recaptcha/api/verify"
-    headers={'Content-type': 'application/x-www-form-urlencoded'}
+    # google_url = "http://www.google.com/recaptcha/api/verify"
+    headers = {'Content-type': 'application/x-www-form-urlencoded'}
 
     # def __init__(self, **kwargs):
     #     super().__init__(**kwargs)
@@ -53,12 +55,18 @@ class RecaptchaWidget(CheckedInputWidget):
                               remoteip=remoteip,
                               challenge=challenge,
                               response=response))
-        h = httplib2.Http(timeout=10)
+        url_components = urlparse(self.url)
+        # h = httplib2.Http(timeout=10)
+        h = http.client.HTTPConnection(url_components["host"], timeout=10)
         try:
-            resp, content = h.request(self.url,
-                                      "POST",
-                                      headers=self.headers,
-                                      body=data)
+            # resp, content = h.request(self.url,
+            #                           "POST",
+            #                           headers=self.headers,
+            #                           body=data)
+            h.request("POST", url_components["path"],
+                      body=data, headers=self.headers)
+            resp = h.getresponse()
+            content = resp.read()
         except AttributeError as e:
             if e=="'NoneType' object has no attribute 'makefile'":
                 ## XXX: catch a possible httplib regression in 2.7 where
@@ -66,11 +74,12 @@ class RecaptchaWidget(CheckedInputWidget):
                 ## XXX sock is still None when makefile is called.
                 raise Invalid(field.schema,
                               "Could not connect to the captcha service.")
-        if not resp['status'] == '200':
+        if not resp.status == '200':
             raise Invalid(field.schema,
                           "There was an error talking to the recaptcha \
                           server{0}".format(resp['status']))
         valid, reason = content.split('\n')
+        # resp.reason?
         if not valid == 'true':
             if reason == 'incorrect-captcha-sol':
                 reason = "Incorrect solution"

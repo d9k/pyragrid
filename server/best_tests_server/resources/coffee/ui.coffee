cbacc529e03e40bf15371409ff1531cc96f00955
d9k <d9k@ya.ru>
2015-10-27 05:47:00 +0300

    UI helper class with PNotify create methods

--- server/best_tests_server/resources/coffee/ui.coffee
+++ server/best_tests_server/resources/coffee/ui.coffee
@@ -1,12 +1,25 @@
 $(document).ready ->
-#  class UI
-#    constructor: () ->
-##    TODO notify helpers
-#    @
+  class UI
+    constructor: () ->
+    @DEF_MESSAGE_SUCCESS: 'Изменение прошло успешно'
+    @DEF_MESSAGE_ERROR: 'Ошибка при изменении'
+    @messageSuccess: (ajaxData) ->
+      ajaxData.message || @.DEF_MESSAGE_SUCCESS
+    @messageError: (ajaxData) ->
+      ajaxData.message || @.DEF_MESSAGE_ERROR
+    @notifyFromSuccessData: (ajaxData) ->
+        new PNotify(
+          title: @.messageSuccess(ajaxData)
+          type: 'success'
+        )
+    @notifyFromErrorData: (ajaxData) ->
+        new PNotify(
+          title: @.messageSuccess(ajaxData)
+          type: 'error'
+        )
 
+  window.UI = UI
   PNotify.prototype.options.styling = "bootstrap3"
   PNotify.prototype.options.opacity = 0.7
   PNotify.prototype.options.hide = true
   PNotify.prototype.options.delay = 4000
-
-  return
--- server/best_tests_server/static/js/ui.js
+++ server/best_tests_server/static/js/ui.js
@@ -1,9 +1,43 @@
 (function() {
   $(document).ready(function() {
+    var UI;
+    UI = (function() {
+      function UI() {}
+
+      UI.DEF_MESSAGE_SUCCESS = 'Изменение прошло успешно';
+
+      UI.DEF_MESSAGE_ERROR = 'Ошибка при изменении';
+
+      UI.messageSuccess = function(ajaxData) {
+        return ajaxData.message || this.DEF_MESSAGE_SUCCESS;
+      };
+
+      UI.messageError = function(ajaxData) {
+        return ajaxData.message || this.DEF_MESSAGE_ERROR;
+      };
+
+      UI.notifyFromSuccessData = function(ajaxData) {
+        return new PNotify({
+          title: this.messageSuccess(ajaxData),
+          type: 'success'
+        });
+      };
+
+      UI.notifyFromErrorData = function(ajaxData) {
+        return new PNotify({
+          title: this.messageSuccess(ajaxData),
+          type: 'error'
+        });
+      };
+
+      return UI;
+
+    })();
+    window.UI = UI;
     PNotify.prototype.options.styling = "bootstrap3";
     PNotify.prototype.options.opacity = 0.7;
     PNotify.prototype.options.hide = true;
-    PNotify.prototype.options.delay = 4000;
+    return PNotify.prototype.options.delay = 4000;
   });
 
 }).call(this);

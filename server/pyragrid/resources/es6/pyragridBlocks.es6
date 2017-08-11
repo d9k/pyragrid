20efefc2c61645a495436327913cdc80ad4104bb
d9k <d9k@ya.ru>
2017-08-11 03:12:00 +0300

    blocks: try to find

--- server/pyragrid/resources/es6/pyragridBlocks.es6
+++ server/pyragrid/resources/es6/pyragridBlocks.es6
@@ -2,7 +2,26 @@ if (typeof window.pyragrid === 'undefined'){
   window.pyragrid = {};
 }
 
-pyragrid.renderBlocks = function() {
-  alert('pyragrid blocks render');
-};
+pyragrid.renderBlocks = function(element) {
+  let findTextNodes = function(element, onTextNode){
+    if (element.tagName === "SCRIPT"){
+      return;
+    }
 
+    let childNodes = element.childNodes;
+    for (let i = 0; i < childNodes.length; i++){
+      let childNode = childNodes[i];
+      if (childNode.nodeType === Node.TEXT_NODE){
+        onTextNode(childNode);
+      } else if (childNode.nodeType === Node.ELEMENT_NODE){
+        findTextNodes(childNode, onTextNode);
+      } else {
+        console.log('nodeType:' + childNode.nodeType);
+      }
+    }
+  };
+
+  findTextNodes(element, function (textNode) {
+    console.log(textNode.textContent);
+  })
+};
--- server/pyragrid/static/js/pyragridBlocks.js
+++ server/pyragrid/static/js/pyragridBlocks.js
@@ -4,6 +4,26 @@ if (typeof window.pyragrid === 'undefined') {
   window.pyragrid = {};
 }
 
-pyragrid.renderBlocks = function () {
-  alert('pyragrid blocks render');
+pyragrid.renderBlocks = function (element) {
+  var findTextNodes = function findTextNodes(element, onTextNode) {
+    if (element.tagName === "SCRIPT") {
+      return;
+    }
+
+    var childNodes = element.childNodes;
+    for (var i = 0; i < childNodes.length; i++) {
+      var childNode = childNodes[i];
+      if (childNode.nodeType === Node.TEXT_NODE) {
+        onTextNode(childNode);
+      } else if (childNode.nodeType === Node.ELEMENT_NODE) {
+        findTextNodes(childNode, onTextNode);
+      } else {
+        console.log('nodeType:' + childNode.nodeType);
+      }
+    }
+  };
+
+  findTextNodes(element, function (textNode) {
+    console.log(textNode.textContent);
+  });
 };

15a44cf646e6bfa693f03ad82641b570f3f00cc7
d9k <d9k@ya.ru>
2017-08-23 08:22:56 +0300

    test_blocks: + global mobx store

--- server/pyragrid/resources/es6/pyragridBlocks.es6
+++ server/pyragrid/resources/es6/pyragridBlocks.es6
@@ -49,6 +49,47 @@ pyragrid.renderBlocks = function(element) {
     );
 };
 
+window.withDefault = mobxStateTree.types.optional;
+
+pyragrid.storeTypeMixins = [];
+
+pyragrid.recreateStore = (modifyInputDataCallback) => {
+  let snapshot = {};
+  if (typeof pyragrid.store !== 'undefined'){
+    snapshot = mobxStateTree.getSnapshot(pyragrid.store);
+  }
+
+  if (typeof modifyInputDataCallback === 'function'){
+    modifyInputDataCallback(snapshot);
+  }
+
+  pyragrid.StoreType = mobxStateTree.types.compose.apply(null, ['Store', ...pyragrid.storeTypeMixins]);
+
+  pyragrid.store = pyragrid.StoreType.create(snapshot);
+};
+
+pyragrid.storeTypeMixins.push( mobxStateTree.types.model(
+  'TestMixin',
+  {
+    testString: withDefault(mobxStateTree.types.string, 'Test string')
+  }
+));
+
+pyragrid.recreateStore((snapshot) => {
+  snapshot.testString = 'Test string 2';
+});
+
+pyragrid.storeTypeMixins.push( mobxStateTree.types.model(
+  'SC2Mixin',
+  {
+    SC2Unit: withDefault(mobxStateTree.types.string, 'lurker')
+  }
+));
+
+pyragrid.recreateStore((snapshot) => {
+  //snapshot.SC2Unit = 'marine';
+});
+
 pyragrid.blockHandlers['hello_world'] = function(appealToWhom){
   return '<p class="helloWorldBlock">Hello, ' + appealToWhom + '!</p>';
 };
--- server/pyragrid/static/js/pyragridBlocks.js
+++ server/pyragrid/static/js/pyragridBlocks.js
@@ -1,5 +1,7 @@
 'use strict';
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 if (typeof window.pyragrid === 'undefined') {
   window.pyragrid = {};
 }
@@ -50,6 +52,41 @@ pyragrid.renderBlocks = function (element) {
   console.log(nunjucksEnv.renderString(element.innerHTML));
   element.innerHTML = nunjucksEnv.renderString(element.innerHTML);
 };
+
+window.withDefault = mobxStateTree.types.optional;
+
+pyragrid.storeTypeMixins = [];
+
+pyragrid.recreateStore = function (modifyInputDataCallback) {
+  var snapshot = {};
+  if (typeof pyragrid.store !== 'undefined') {
+    snapshot = mobxStateTree.getSnapshot(pyragrid.store);
+  }
+
+  if (typeof modifyInputDataCallback === 'function') {
+    modifyInputDataCallback(snapshot);
+  }
+
+  pyragrid.StoreType = mobxStateTree.types.compose.apply(null, ['Store'].concat(_toConsumableArray(pyragrid.storeTypeMixins)));
+
+  pyragrid.store = pyragrid.StoreType.create(snapshot);
+};
+
+pyragrid.storeTypeMixins.push(mobxStateTree.types.model('TestMixin', {
+  testString: withDefault(mobxStateTree.types.string, 'Test string')
+}));
+
+pyragrid.recreateStore(function (snapshot) {
+  snapshot.testString = 'Test string 2';
+});
+
+pyragrid.storeTypeMixins.push(mobxStateTree.types.model('SC2Mixin', {
+  SC2Unit: withDefault(mobxStateTree.types.string, 'lurker')
+}));
+
+pyragrid.recreateStore(function (snapshot) {
+  //snapshot.SC2Unit = 'marine';
+});
 
 pyragrid.blockHandlers['hello_world'] = function (appealToWhom) {
   return '<p class="helloWorldBlock">Hello, ' + appealToWhom + '!</p>';
